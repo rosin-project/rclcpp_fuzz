a64f17a335bc94e12c759af43e9968817a6f3f57
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-01-10 20:01:28 +0100

    fuzz.sh: Improve verbosity and error messages

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
+# colors for more readability
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
 export ROS_DIST=/opt/ros/eloquent
 
 # ABSOLUTE path to the ROS_WS
 export ROS_WS=/opt/ros_ws
 # Name of the package we are interested in
 
-echo 1. Configuration requirements of afl-fuzz
+STEP=1
+
+msg () {
+  printf "\n * ${CYAN}[${STEP}]${WHITE} $1 ${NC}\n\n"
+  let "STEP += 1"
+}
+
+msg "Configuration requirements of afl-fuzz"
 echo core > /proc/sys/kernel/core_pattern
 cd /sys/devices/system/cpu
 echo performance | tee cpu*/cpufreq/scaling_governor
 cd ${ROS_WS}
 
 # If llvm-cov is named gcov then it acts as if it received the gcov verb
-echo
-echo 2. Disguise llvm-cov as gcov for lcov
+
+msg "Disguise llvm-cov as gcov for lcov"
 ln `which llvm-cov-4.0` ${ROS_WS}/gcov -sfv
 GCOVTOOL="--gcov-tool ${ROS_WS}/gcov"
 
@@ -25,8 +37,9 @@ LCOVDIR=build/
 # If visible on the host you can just view in browser
 REPORT=src/rclcpp_fuzz/fuzz/Coverage_Report
 # How long time we allow afl-fuzz to run
-DURATION=100s
-echo Fuzzing duration is set to ${DURATION}
+DURATION=10s
+msg "Fuzzing duration is set to ${DURATION}"
+
 
 export CC=afl-clang 
 export CXX=afl-clang++ 
@@ -34,76 +47,84 @@ export CXXFLAGS="-fprofile-arcs -ftest-coverage -g -O0"
 export CMAKE_EXE_LINKER_FLAGS="-fprofile-arcs -ftest-coverage"
 export CMAKE_BUILD_TYPE="Coverage"
 export AFL_USE_ASAN=1
-export VERBOSE=1
+# Change to 1 to see the compilation messages
+export VERBOSE=0
 
-echo source ${ROS_DIST}/setup.sh
+EVENT_HANDLERS="--event-handlers console_direct+"
+if [ ${VERBOSE} -eq "0" ]; then
+  EVENT_HANDLERS=0;
+fi
+
+msg "source ${ROS_DIST}/setup.sh"
 . ${ROS_DIST}/setup.sh
+
 
 # We assume that rclcpp_fuzz is in the workspace, but let's get its
 # dependencies in, and let's compile. This is a convenience step if the
 # docker container is just started from an empty rclcpp build.
 # Perhaps these steps will go to the dockerfile eventually.
-echo
-echo 3. Installing dependencies for the client/server project
+
+msg "Installing dependencies for the client/server project"
 rosdep install --from-paths src -y
 
-echo
-echo 4. Compiling the client server project
-colcon build --event-handlers console_direct+
+
+msg "Compiling the client server project"
+colcon build ${EVENT_HANDLERS} 
+
 
 # This makes the build client and server available to ros2 run
-echo
-echo 5. source ${ROS_WS}/install/setup.sh
+msg "source ${ROS_WS}/install/setup.sh"
 . ${ROS_WS}/install/setup.sh
 
-echo
-echo "6. Removing stale fuzzing results (might not be desirable)"
+
+msg "Removing stale fuzzing results (might not be desirable)"
 rm ${ROS_WS}/src/rclcpp_fuzz/fuzz/out/* -Rfv
 
-echo
-echo "7. Removing stale coverage results (might not be desirable)"
+
+msg "Removing stale coverage results (might not be desirable)"
 find . -name "*.gcda" -print0 | xargs -0 rm
 
-echo
-echo "8. Clean up lcov state"
+
+msg "Clean up lcov state"
 lcov ${GCOVTOOL} -directory ${LCOVDIR} --zerocounters
 
-echo
-echo "9. Create baseline to make sure untouched files show up in the report"
+
+msg "Create baseline to make sure untouched files show up in the report"
 lcov ${GCOVTOOL} -c -i -d ${LCOVDIR} -o ${REPORT}.base
 
-echo
-echo "10. Starting the server"
+
+msg "Starting the server"
 ros2 run rclcpp_fuzz server > /dev/null &
 
-echo
-echo "11. Starting afl-fuzz for ${DURATION} time"
+
+msg "Starting afl-fuzz for ${DURATION} time"
 # m - none (you can replace none with the memory size in megabytes)
 timeout ${DURATION} afl-fuzz -m none -t 2000 -i ${ROS_WS}/src/rclcpp_fuzz/fuzz/in/ \
     -o ${ROS_WS}/src/rclcpp_fuzz/fuzz/out/ -- \
     install/rclcpp_fuzz/lib/rclcpp_fuzz/client
 
-echo
-echo "12. Killing the server (the client and afl-fuzz should already be dead)"
+
+msg "Killing the server (the client and afl-fuzz should already be dead)"
 pkill -e server
 
-echo
-echo 13.Capturing lcov counters and generating report
+
+msg "Capturing lcov counters and generating report"
 lcov ${GCOVTOOL} --directory ${LCOVDIR} \
      --capture --output-file ${REPORT}.info
 
-echo
-echo 14. Add baseline counters
+
+msg "Add baseline counters"
 lcov ${GCOVTOOL} -a ${REPORT}.base -a ${REPORT}.info \
      --output-file ${REPORT}.total
 
-echo
-echo 15. Consolidate and clean
+
+msg "Consolidate and clean"
 lcov ${GCOVTOOL} --remove ${REPORT}.total \
      --output-file ${REPORT}.info.cleaned
 
-echo
-echo 16. Generate HTML report
+
+msg "Generate HTML report"
 genhtml -o ${REPORT} ${REPORT}.info.cleaned
+
 
 
