38379aff25f81a28dbcc6f9d8515f92a5ed73ae9
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-01-10 14:55:38 +0100

    client: Read from stdin to try with fuzzing

--- src/add_two_ints_client.cpp
+++ src/add_two_ints_client.cpp
@@ -14,22 +14,20 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  if (argc != 3) {
-
-      RCLCPP_INFO (rclcpp::get_logger ("rclcpp"), 
-          "usage: add_two_ints_client X Y");
-      return 1;
-  }
-
   std::shared_ptr<rclcpp::Node> node = 
     rclcpp::Node::make_shared("add_two_ints_client");
 
   rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
     node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
 
-  auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-  request->a = atoll (argv[1]);
-  request->b = atoll (argv[2]);
+  long a, b;
+  std::cin >> a;
+  std::cin >> b;
+
+  auto request = 
+    std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+  request->a = a;
+  request->b = b;
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
