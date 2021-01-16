da686e72f1be84c4477dfc7d8cf4aee9c51f7dd3
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-01-16 11:27:45 +0100

    fuzz_target: fix the hangs in the service fuzzer

--- src/fuzz_target.cpp
+++ src/fuzz_target.cpp
@@ -85,41 +85,37 @@ bool getString(std::string& s, int8_t size) {
 // 1. Server
 //
 void fuzz_server() {
-  std::shared_ptr<rclcpp::Node> node = 
-    rclcpp::Node::make_shared("add_two_ints_client");
-
-  rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
 
   long a, b;
-  while (getInt64(a) && getInt64(b)) {
-    auto request = 
-      std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-    request->a = a;
-    request->b = b;
+  auto node = rclcpp::Node::make_shared ("add_two_ints_client");
 
-    while (!client->wait_for_service(1s)) {
+  auto client = 
+    node->create_client<example_interfaces::srv::AddTwoInts> ("add_two_ints");
+
+    while (!client->wait_for_service ()) {
+
       if (!rclcpp::ok()) {
         RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
         return;
       }
+
       RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
     }
 
-    auto result = client->async_send_request(request);
-    
-    // Wait for the result.
-    if (rclcpp::spin_until_future_complete (node, result) ==
-      rclcpp::executor::FutureReturnCode::SUCCESS)
-    {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
-    } 
-    else 
-    {
-      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
-    }
+  while (getInt64(a) && getInt64(b)) {
+
+    auto request = 
+      std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+
+    request->a = a;
+    request->b = b;
+
+    auto result = client->async_send_request (request);
+    rclcpp::spin_until_future_complete (node, result);
+
   }
 }
+
 
 //
 // 2. Subscriber
